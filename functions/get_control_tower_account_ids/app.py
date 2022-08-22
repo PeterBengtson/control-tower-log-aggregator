9805ffbf8c42ae2c4cd0c25c0378e42f6c4de531
Peter Bengtson <peter.bengtson@schibsted.com>
2022-08-22 12:42:21 +0200

    Rename of lambda. Preparation for split paths.

--- functions/get_control_tower_account_ids/app.py
+++ functions/get_control_tower_account_ids/app.py
@@ -13,12 +13,25 @@ def lambda_handler(data, _context):
     client.head_bucket(Bucket=bucket_name)
     print("Bucket exists.")
 
+    print(f"Checking whether the CloudTrail log structure is that from Control Tower v3.0 or higher...")
+
+    prefix = f'{ORG_ID}/AWSLogs/{ORG_ID}/'
     response = client.list_objects_v2(
         Bucket=bucket_name,
         Delimiter='/',
-        Prefix=f'{ORG_ID}/AWSLogs/'
+        Prefix=prefix
     )
+    account_prefixes = list(map(lambda x: x['Prefix'], response['CommonPrefixes']))
+    if account_prefixes:
+        print(f"Version 3.0+ detected: logs found under {prefix}.")
+        return account_prefixes
 
+    prefix = f'{ORG_ID}/AWSLogs/'
+    response = client.list_objects_v2(
+        Bucket=bucket_name,
+        Delimiter='/',
+        Prefix=prefix
+    )
     account_prefixes = list(map(lambda x: x['Prefix'], response['CommonPrefixes']))
     print(account_prefixes)
     return account_prefixes
